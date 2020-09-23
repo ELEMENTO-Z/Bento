7c1bd475baf4d9a1371aa659ab5c56c704be2aaf
ELEMENTO-Z <52634209+ELEMENTO-Z@users.noreply.github.com>
2020-09-23 18:53:35 -0400

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -2,11 +2,11 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'Emmanuel';
-var lateTxt = 'GG! ';
-var morningTxt = 'Buenos Dias! ';
-var afterTxt = 'Buenas Tardes! ';
-var evenTxt = 'Buenas Noches! ';
+var name = 'Emmanuel!';
+var lateTxt = 'DEMACIADO TARDE A DORMIR ALV!  ';
+var morningTxt = 'Buenos Dias ';
+var afterTxt = 'Buenas Tardes ';
+var evenTxt = 'Buenas Noches ';
 
 if (Hr >= 0 && Hr < 6) {
     document.getElementById('greetings').innerText = lateTxt + name;
